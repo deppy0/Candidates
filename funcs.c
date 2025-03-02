765b251eb267746deee9103ddd22494b37013e24
deppy0 <dependant1b@gmail.com>
2025-03-02 13:04:11 +0800

    clarified param name

--- funcs.c
+++ funcs.c
@@ -7,14 +7,14 @@ void swap(candidate_p ptrcand1, candidate_p ptrcand2)
 	*ptrcand2 = temp;	
 }
 
-void sortByRating(candidate_l_p list, int boolIncreasing)
+void sortByRating(candidate_l_p list, int boolHighestOnTop)
 {
 	int i, j, mx;
 	for (i = 0; i < list->count - 1; i++)
 	{
 		mx = i;
 		for (j = i + 1; j < list->count; j++)
-			if (boolIncreasing)
+			if (boolHighestOnTop)
 				if (list->candidates[j].confidence > list->candidates[mx].confidence)
 					mx = j;
 				else;
@@ -34,11 +34,11 @@ void sortAlphabetical(candidate_l_p list, int boolSortAtoZ)
 		m = i;
 		for (j = i + 1; j < list->count; j++)
 			if (!boolSortAtoZ)
-				if (strcmp(list->candidates[m].name.last, list->candidates[j].name.last) > 0)
+				if (strcmp(list->candidates[m].name.last, list->candidates[j].name.last) < 0)
 					m = j;
 				else;
 			else
-				if (strcmp(list->candidates[m].name.last, list->candidates[j].name.last) < 0)
+				if (strcmp(list->candidates[m].name.last, list->candidates[j].name.last) > 0)
 					m = j;
 				else;
 		swap(&list->candidates[i], &list->candidates[m]);
